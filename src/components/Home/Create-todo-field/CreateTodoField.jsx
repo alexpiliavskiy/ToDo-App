636ecb86d89f02165e3ceec9a9c9dc0b6b6479d0
Olexander <alex.pilyavskiy03@gmail.com>
2024-06-04 16:58:41 +0300

    fixed add task

--- src/components/Home/Create-todo-field/CreateTodoField.jsx
+++ src/components/Home/Create-todo-field/CreateTodoField.jsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 
 const CreateTodoField = ({ setTodos }) => {
   const [title, setTitle] = useState("");
+  const [isError, setIsError] = useState(false);
 
-  const addTodo = (title) => {
-    if (!title) return;
+  const addTodo = () => {
+    if (!title.trim()) {
+      setIsError(true);
+      return;
+    }
+    setIsError(false);
     setTodos((prev) => [
       {
         id: new Date(),
@@ -16,16 +21,38 @@ const CreateTodoField = ({ setTodos }) => {
     setTitle("");
   };
 
+  const handleInputChange = (event) => {
+    setTitle(event.target.value);
+    if (isError) {
+      setIsError(false);
+    }
+  };
+
   return (
-    <div className="flex items-center justify-between rounded-2xl border-gray-800 border-2 px-5 py-3 w-full mb-10">
-      <input
-        type="text"
-        onChange={(event) => setTitle(event.target.value)}
-        value={title}
-        onKeyDown={(event) => event.key === "Enter" && addTodo(title)}
-        className="bg-transparent w-full border-none outline-none"
-        placeholder="Add a task..."
-      />
+    <div className="flex flex-col items-center justify-between rounded-2xl border-gray-800 border-2 px-5 py-3 w-full mb-10">
+      <div className="flex items-center w-full">
+        <input
+          type="text"
+          onChange={handleInputChange}
+          value={title}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              addTodo();
+            }
+          }}
+          className="bg-transparent w-full border-none outline-none"
+          placeholder="Add a task..."
+        />
+        <button
+          onClick={addTodo}
+          className="bg-green-500 text-white py-1 px-4 rounded hover:bg-green-900 transition-colors ease-in duration-300"
+        >
+          Add
+        </button>
+      </div>
+      {isError && (
+        <span className="text-red-700 font-bold">Field must be not empty!</span>
+      )}
     </div>
   );
 };
